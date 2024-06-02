3d46bcfb540e16fd6338ad63f99e267973b65632
Ricardo <20230620@ricaldone.edu.sv>
2024-06-02 12:49:22 -0600

    agregue operadores aritmeticos

---
+++ operadores/op_aritmeticos.py
@@ -0,0 +1,29 @@
+#suma y resta (+ y -)
+suma =  12 + 5
+print(suma)
+
+resta = 12 - 5
+print(resta)
+
+#multiplicacion y division (* y /)
+multiplicacion = 12 * 5
+print(multiplicacion)
+
+division = 12 / 5 #siempre nos devolvera un dato float 
+print(division)
+
+#potenciacion (exponente) (**)
+exponente = 12 ** 5
+print(exponente)
+
+#division baja (//)
+division_baja = 12 // 5 #devuelve entero redondeado hacia abajo
+print(division_baja)
+
+#resto o modulo
+resto = 12 % 5
+print(resto)
+
+#type (dato) nos devuelve que tipo de dato es
+tipo_de_dato = type(5) 
+print(tipo_de_dato)

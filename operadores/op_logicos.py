8457a1a144896bb8c90a782afc4bdb95caa263b1
Ricardo <20230620@ricaldone.edu.sv>
2024-06-02 19:41:19 -0600

    agregue los operadores logicos

---
+++ operadores/op_logicos.py
@@ -0,0 +1,18 @@
+#AND
+
+resultado = True & True #verdadero
+resultado2 = False & True #Falso
+resultado3= True & False #Falso
+resultado4 = False & False #Falso
+
+#OR
+
+resultado5 = True | True #Verdadero
+resultado6 = False | True #Verdadero
+resultado7 = True | False #Verdadero
+resultado8 = False | False #Falso
+
+#NOT
+
+resultado9 = not True #Falso
+resultado10 = not False #Falso

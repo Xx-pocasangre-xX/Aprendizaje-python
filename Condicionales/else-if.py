1156d18adab0c1e70b0aa6aeadc548e1e6c4a64e
Ricardo <20230620@ricaldone.edu.sv>
2024-06-02 19:30:40 -0600

    agregue el elif

--- Condicionales/else-if.py
+++ Condicionales/else-if.py
@@ -1,10 +1,14 @@
 #con la condicional (elif) si no se ejecuta el if por que no es valido se va ejecutar la siguiente y en este ejemplo 
 #estariamos diciendo que si no tiene 10000 dolares o mas puedes ser que tenga 1000 o mas 
-dinero_ingreso = 5000
+dinero_ingreso = 10000
+dinero_gasto = 4000
 
+#otra de la maneras en la cual podemos hacer un condicion es poniendo un if en otro if (ifs anidados)
 if dinero_ingreso >= 10000 :
-    print("Tienes bastante dinero")
-    
+    if dinero_ingreso - dinero_gasto >= 3000:
+        print("Tienes bastante dinero")
+    else:
+        print("estas gastando demasiado dinero")
     
 elif dinero_ingreso >= 1000:
     print("estas bien economicamente en el pais")
